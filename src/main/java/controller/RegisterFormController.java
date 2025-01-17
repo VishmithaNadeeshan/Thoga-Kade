23683e460b47334fa36779682478230b5ce45e03
VishmithaNadeeshan <vishmithanadeeshan20041111@gmail.com>
2025-01-18 00:21:31 +0530

    login UI

---
+++ src/main/java/controller/RegisterFormController.java
@@ -0,0 +1,65 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RegisterFormController {
+
+    @FXML
+    private JFXPasswordField txtConfirmPassword;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    void btnRegisterCustomer(ActionEvent event) throws SQLException {
+        String SQL = "INSERT INTO users (username,email,password) VALUES(?,?,?)";
+
+        if (txtPassword.getText().equals(txtPassword.getText())){
+
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            ResultSet resultSet = connection
+                    .createStatement()
+                    .executeQuery("SELECT * FROM users WHERE email=" + "'" + txtEmail.getText() + "'");
+
+            if (!resultSet.next()){
+                User user = new User(
+                        txtUsername.getText(),
+                        txtEmail.getText(),
+                        txtPassword.getText()
+                );
+
+                PreparedStatement psTm = connection.prepareStatement(SQL);
+                psTm.setString(1,user.getUsername());
+                psTm.setString(2,user.getEmail());
+                psTm.setString(3,user.getPassword());
+                psTm.executeUpdate();
+
+            }else{
+                new Alert(Alert.AlertType.ERROR,"user found!").show();
+            }
+        }else{
+            new Alert(Alert.AlertType.ERROR,"Check your password!!").show();
+        }
+    }
+
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class User {
+    private String username;
+    private String Email;
+    private String Password;
+}
