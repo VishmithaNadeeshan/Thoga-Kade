619a8c527d461c1be4a439e1ab2bb066b6918475
VishmithaNadeeshan <vishmithanadeeshan20041111@gmail.com>
2025-01-18 01:13:18 +0530

    Password Encrypt

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -2,9 +2,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jasypt.util.text.BasicTextEncryptor;
 
 public class Starter extends Application {
     public static void main(String[] args) {
+        String password="vishmitha123";
+        String key="#12235435";
+
+        BasicTextEncryptor basicTextEncryptor = new BasicTextEncryptor();
+        basicTextEncryptor.setPassword(key);
+
+        String encrypt = basicTextEncryptor.encrypt(password);
+        System.out.println("Encrypt Password : "+encrypt);
+
+        String decrypt = basicTextEncryptor.decrypt(encrypt);
+        System.out.println("Dycrypt Password : "+decrypt);
+
         launch();
     }
     @Override
--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -2,13 +2,19 @@ package controller;
 
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import model.User;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginFormController {
 
@@ -19,8 +25,29 @@ public class LoginFormController {
     private JFXPasswordField txtPassword;
 
     @FXML
-    void btnLoginOnAction(ActionEvent event) {
+    void btnLoginOnAction(ActionEvent event) throws SQLException, IOException {
+        String SQL = "SELECT * FROM users WHERE email ="+"'"+txtEmail.getText()+"'";
+        Connection connection = DBConnection.getInstance().getConnection();
+        ResultSet resultSet = connection.createStatement().executeQuery(SQL);
+        if(resultSet.next()){
+            User user = new User(
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4)
+            );
+            if (user.getPassword().equals(txtPassword.getText())) {
+                Stage stage = new Stage();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"))));
+                stage.show();
 
+            }else {
+                new Alert(Alert.AlertType.ERROR, "Password is incorrect!").show();
+            }
+
+            System.out.println(user);
+        }else {
+            new Alert(Alert.AlertType.ERROR, "User not found!").show();
+        }
     }
 
     @FXML
